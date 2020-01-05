722c70c69f58c923fa755fed670eb5b1eab950f7
Emily <44551263+emjwalter@users.noreply.github.com>
2020-01-05 20:44:41 +0000

    1st codebar
    18 Dec 2019 @ Google with Katherine

---
+++ ex1.py
@@ -0,0 +1,20 @@
+print("Hello"" & " "Goodbye")
+
+## Maths practice ##
+print(10 / 3)
+print(5 * 2)
+print(3 ** 4)
+
+
+## INPUT IF/ELIF ##
+
+coffee = input("How many cups of coffee have you drunk today? ")
+
+if int(coffee) > 4:
+    print("You have a coffee problem")
+else:
+    print("You do not have a coffee problem")
+
+
+## LISTS ##
+places_to_visit = ["Mexico", "Portugal", "Kenya", "Nepal", "New Zealand"]
